1435f87c28f8c09d7ed039afa6562d1a3c24be37
ConnorSewell <ctrsewell@dundee.ac.uk>
2017-02-09 23:24:53 +0000

    Initial commit

---
+++ src/main/java/com/example/testproject/mobile_phone_honours_project_connor_sewell/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.testproject.mobile_phone_honours_project_connor_sewell;
+
+import android.content.Context;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+
