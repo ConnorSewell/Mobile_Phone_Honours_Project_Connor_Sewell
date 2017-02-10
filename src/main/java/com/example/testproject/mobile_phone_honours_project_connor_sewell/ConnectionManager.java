a2228944544fac66992062c057099586058cfc10
ConnorSewell <ctrsewell@dundee.ac.uk>
2017-02-10 02:09:34 +0000

    Building more of ConnectionManager class. Basic connection code implemented. Not tested.

--- src/main/java/com/example/testproject/mobile_phone_honours_project_connor_sewell/ConnectionManager.java
+++ src/main/java/com/example/testproject/mobile_phone_honours_project_connor_sewell/ConnectionManager.java
@@ -2,6 +2,11 @@ package com.example.testproject.mobile_phone_honours_project_connor_sewell;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.ParcelFileDescriptor;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  * Created by Connor on 10/02/2017.
@@ -9,11 +14,8 @@ import android.os.AsyncTask;
  * Code for networking taken from: https://developer.android.com/guide/topics/connectivity/wifip2p.html#creating-app
  * ^ Accessed: 10/02/2017 @ 01:29
  */
-
-
 public class ConnectionManager extends AsyncTask<Void, Void, String>
 {
-
     Context context;
 
     public ConnectionManager(Context context)
@@ -26,11 +28,32 @@ public class ConnectionManager extends AsyncTask<Void, Void, String>
     @Override
     protected String doInBackground(Void... params)
     {
+        ServerSocket sv;
+        Socket client;
+        InputStream inputStream;
+
+        //Need to update statements to catch exact errors
         try
+        {
+            sv = new ServerSocket(8888);
+            client = sv.accept();
+            inputStream = client.getInputStream();
+        }
+        catch(Exception e)
         {
             return "CannotConnect";
         }
-        catch(Exception e){}
+
+        try
+        {
+            //Receive input streams, then close stream.
+
+            sv.close();
+        }
+        catch(Exception e)
+        {
+            return "StreamInterrupted";
+        }
 
         //Return if stream ended
         return "StreamEnded";
--- src/main/java/com/example/testproject/mobile_phone_honours_project_connor_sewell/MainActivity.java
+++ src/main/java/com/example/testproject/mobile_phone_honours_project_connor_sewell/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         ConnectionManager cm = new ConnectionManager(this);
+
     }
 
 }
